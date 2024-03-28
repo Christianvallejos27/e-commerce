3586f2a892d202f9082f358e90dc1c43e8c56535
unknown <christianvallejos74@yahoo.com>
2024-03-27 20:49:40 -0400

    made changes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const allproducts = await product.findAll({
+    const allproducts = await Product.findAll({
       include:[Product]
     })
     res.status(200).json(allproducts)
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const oneproduct = await product.findOne({
+    const oneproduct = await Product.findOne({
       where:{
         id:req.params.id
       },
@@ -50,7 +50,7 @@ router.post('/', async (req, res) => {
     }
   */
     try {
-      const createproduct = await product.create(req.body)
+      const createproduct = await Product.create(req.body)
       res.status(200).json(createproduct)
     } catch (error) {
       console.error(error)
@@ -82,7 +82,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update product data
   try {
-    const updateproduct = await product.update(req.body,{
+    const updateproduct = await Product.update(req.body,{
       where:{
         id:req.params.id
       }
@@ -137,7 +137,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
   try {
-    const deleteproduct = await product.destroy({
+    const deleteproduct = await Product.destroy({
       where:{
         id:req.params.id
       }
